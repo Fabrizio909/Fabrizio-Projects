5ef22e0759d268930b8aa57a498efbd474561b2f
Fabrizio909 <fabrizioandreis06@gmail.com>
2025-06-23 15:14:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const esito = document.getElementById('esito');
 let media = ((creditmark/2)+firstmark+secondmark)/3;
 let stima = creditmark + firstmark + secondmark + media;
 let picture = document.getElementById('picture');
+let nomeGestito = false;
 let imageSet = false;
 picture.style.display = "none";
 picture.src = "";
@@ -21,35 +22,50 @@ if (!name.includes(" ")) {
 
 if (name === "beatrice bertazza") {
    message = "Uscirai con un buon 90/100, Luana è rimasta impressa dal tuo tema e deciderà di premiarti. Daje";
+   nomeGestito = true;
 } else if (name === "ingrid busetti") {
    message = "Luana sarà sconcertata dal tuo voto di diritto. Ma niente paura, Portolan e Casagrande saranno lì a sostenerti e decideranno di premiare il tuo impegno. Forza Ingrid!";
    stima -= 30;
+   nomeGestito = true;
 } else if (name === "luca dagostin") {
    message = "I prof rimarranno stupefatti dalla tua abilità di utilizzare l'arte del Freestyle. Uscirai con un ottimo voto.";
+   nomeGestito = true;
 } else if (name === "marco depeder") {
    message = "Il tuo orale partirà bene, ma ad un certo punto Menapace ti interromperà per correggerti. A quel punto tu giustamente inizierai ad insultarla con termini tipo Muori, Esplodi, Sciopa. Ti prego non farlo o non arriverai al 60. Se riuscirai a trattenerti avrai un voto attorno al 65. Ti si vuole bene Marco <3" ;
+   nomeGestito = true;
 } else if (name === "nicola gabardi") {
    message = "Agli scritti hai stupito tutti i professori. Peccato che ti ritroverai Casagranda agli orali che ti accuserà di essere eccentrico e supponente. Ti leverà qualche voto ma alla fin dei conti il tuo voto sarà comunque tra l'80 e il 90.";
+   nomeGestito = true;
 } else if (name === "angela larentis") {
    message = "Non sai manco fare l'impostore su Among Us, figuriamoci sostenere un esame di Stato. Si scherza Angela, te la caverai alla grande e uscirai con un buon voto finale.";
+   nomeGestito = true;
 } else if (name === "edoardo paci") {
    message = "Ti uscirà uno spunto sulla guerra tra Israele e Palestina. Insomma lo spunto perfetto. Con la tua conoscenza e abilità retorica stupirai l'intera commissione. Grande Edo <3";
+   nomeGestito = true;
 } else if (name === "federico paternoster") {
    message = "I professori ti accuseranno di essere un comunista, ancora prima che tu possa iniziare a parlare. Tuttavia con la tua abilità retorica e conoscenza della filosofia riuscirai a dimostrare loro che hanno torto: tu sei troppo joker per essere comunista.";
+   nomeGestito = true;
 } else if (name === "veronica paternoster") {
    message = "I prof rimarranno inondati da tutte le nozioni che dimostrerai di sapere. Uscirai con un ottimo voto, e in ogni caso sarai contenta di esserti liberata del Liceo Russell.";
+   nomeGestito = true;
 } else if (name === "andrea sampieri") {
    message = "Ti uscirà uno spunto tratto da Pulp Fiction. Insomma, lo spunto perfetto per te. Lascerai a bocca aperta tutta la commissione e riuscirai a convertire Portolan alla religione del Quentinesimo.";
+   nomeGestito = true;
 } else if (name === "yara yousfi") {
    message = "Con apollineo e dionisiaco stupirai l'intera commissione. Casagrande muto, ottimo voto garantito.";
+   nomeGestito = true;
 } else if (name === "andrea zanetti") {
    message = "Il professore di italiano ti farà i complimenti per l'eccellente voto di italiano. Inoltre, appena scoprirà che fai parte delle BR, ti farà i complimenti per la missione Aldo Moro. 100/100 con Lode, daje così. Ah, sei stato allallato.";
+   nomeGestito = true;
 } else if (name === "victoria zappini") {
    message = "Menapace ti farà uno dei suoi soliti interrogatori. Ma alla fine andrà tutto bene. Forza Vic";
+   nomeGestito = true;
 } else if (name === "fabrizio andreis") {
    message = "Fabrizio ha creato questo programma, non so dirti con quale voto uscirà ma mi ha detto che non gliene frega un cazzo del voto.";
+   nomeGestito = true;
 } else if (name === "marco latino") {
-   message = "Latino? L'esame di maturità l'ha fatto tanti anni fa. Gli era andato di merda, ma questo è un altro discorso.";  
+   message = "Latino? L'esame di maturità l'ha fatto tanti anni fa. Gli era andato di merda, ma questo è un altro discorso.";
+   nomeGestito = true;
 } 
 
 if (name === "federico smash") {
@@ -58,6 +74,7 @@ if (name === "federico smash") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "latino gangster") {
@@ -66,6 +83,7 @@ if (name === "latino gangster") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 } 
 
 if (name === "zanetti palestrato") {
@@ -74,6 +92,7 @@ if (name === "zanetti palestrato") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "seppi palestrato") {
@@ -82,6 +101,7 @@ if (name === "seppi palestrato") {
    picture.style.display = "block";
      imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "portopalo carletto") {
@@ -90,6 +110,7 @@ if (name === "portopalo carletto") {
    picture.style.display = "block";
     imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "federico interista") {
@@ -98,6 +119,7 @@ if (name === "federico interista") {
    picture.style.display = "block";
     imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 } 
 
 if (name === "zattoni erutta") {
@@ -106,6 +128,7 @@ if (name === "zattoni erutta") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "sampieri sampdoria") {
@@ -114,6 +137,7 @@ if (name === "sampieri sampdoria") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "federico mafioso") {
@@ -122,6 +146,7 @@ if (name === "federico mafioso") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "michele retrosi") {
@@ -130,6 +155,7 @@ if (name === "michele retrosi") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (name === "vitelli sborelli") {
@@ -138,12 +164,18 @@ if (name === "vitelli sborelli") {
    picture.style.display = "block";
    imageSet = true;
    esito.textContent = "";
+   nomeGestito = true;
 }
 
 if (!imageSet) {
 picture.src = "";
 picture.style.display = "none";
+}
+
+if (nomeGestito && !imageSet) {
 esito.textContent = `Scherzi a parte, ecco la stima del voto finale calcolata matematicamente in base ai voti e crediti inseriti: ${stima.toFixed(2)}`;
+} else {
+esito.textContent = "";
 }
 
 output.textContent = message;
